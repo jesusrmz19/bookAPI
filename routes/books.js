dfacfff3d09297fe15c72c5951671089772454c4
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-10-13 17:24:24 -0500

    Add post function to submit a book

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,25 @@ const router = express.Router();
 
 // ROUTES
 
+// GET ALL BOOKS
 router.get('/', async (req, res) => {
   try {
     const books = await Book.find();
     res.json(books);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// SUBMIT A BOOK
+router.post('/', async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+  });
+  try {
+    const savedBook = await book.save();
+    res.json(savedBook);
   } catch (err) {
     res.json({ message: err });
   }
