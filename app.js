f7282a0aa8868ca8d24296af7300882d457a4fe0
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-10-13 16:55:05 -0500

    first commit of the new book API

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+// IMPORT ROUTES
+const postsRoute = require('./routes/posts');
+
+// MIDDLEWARE ROUTE
+app.use('/posts', postsRoute);
+
+app.get('/', (req, res) => {
+  res.send('We are home ');
+});
+
+// CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, () =>
+  console.log('Connected to DB')
+);
+
+// LISTEN TO THE SERVER
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// ROUTES
+
+// THIS ONE GET'S BACK ALL THE POSTS
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// SUBMITS THE POSTS
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+  // CODIGO SIN ASYNC AWAIT
+  //   post
+  //     .save()
+  //     .then((data) => {
+  //       res.json(data);
+  //     })
+  //     .catch((err) => {
+  //       res.json({ message: err });
+  //     });
+});
+
+// GET A SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// DELETE A POST
+router.delete('/:postId', async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// UPDATE A POST
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        $set: {
+          title: req.body.title,
+        },
+      }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
