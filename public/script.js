dd3c692f5ef4a839e45b7f06667956b3b2128311
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-10-22 17:31:33 -0500

    Change use in main page

--- public/script.js
+++ public/script.js
@@ -5,37 +5,36 @@ const dataElem = document.getElementById('data');
 const bookForm = document.getElementById('book_form');
 const hintBtns = document.querySelectorAll('.hint_btns');
 const input = document.getElementById('input_name');
-const example = document.querySelector('.example');
-
-const createHTML = async () => {
-  try {
-    const res = await fetch(`${URL}/617206c278b8cb2f1fa4d0a1`);
-    const data = await res.json();
-    const { imgUrl, title, author, pages, dateRead } = data;
-    const html = `
-      <div class="example_img">
-        <img src="${imgUrl}" />
-      </div>
-      <div class="example_text">
-        <p class="example_text__title">${title}</p>
-        <p class="example_text__author">${author}</p>
-        <div class="example_subtext">
-          <p class="example_subtext__pages">${pages} pages</p>
-          <p class="example_subtext__read">Ready by: ${dateRead}</p>
-        </div>
-      </div>
-    `;
-    example.insertAdjacentHTML('afterbegin', html);
-  } catch (err) {
-    console.error(err);
-  }
-};
+const example = document.querySelector('.example_section');
 
 const getData = async (condition) => {
   try {
     const res = await fetch(`${URL}${condition}`);
     const data = await res.json();
     dataElem.innerHTML = JSON.stringify(data, undefined, 2);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const createHTML = async () => {
+  try {
+    const res = await fetch(`${URL}/author/Ken Follet`);
+    const data = await res.json();
+    data.forEach((elem) => {
+      const html = `
+      <article class="example_book">
+        <div class="example_img">
+          <img src="${elem.imgUrl}" alt="Cover of ${elem.title} by ${elem.author}" />
+        </div>
+        <div class="example_text">
+          <p class="example_text__title">${elem.title}</p>
+          <p class="example_text__author">${elem.author}</p>
+        </div>
+      </article>
+    `;
+      example.insertAdjacentHTML('afterbegin', html);
+    });
   } catch (err) {
     console.error(err);
   }
