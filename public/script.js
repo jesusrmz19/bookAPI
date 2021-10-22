26288ac53d2ec7061abeec1bf5317d73fee15fbb
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-10-21 21:01:04 -0500

    Creat example usage on main page

--- public/script.js
+++ public/script.js
@@ -5,6 +5,31 @@ const dataElem = document.getElementById('data');
 const bookForm = document.getElementById('book_form');
 const hintBtns = document.querySelectorAll('.hint_btns');
 const input = document.getElementById('input_name');
+const example = document.querySelector('.example');
+
+const createHTML = async () => {
+  try {
+    const res = await fetch(`${URL}/617206c278b8cb2f1fa4d0a1`);
+    const data = await res.json();
+    const { imgUrl, title, author, pages, dateRead } = data;
+    const html = `
+      <div class="example_img">
+        <img src="${imgUrl}" />
+      </div>
+      <div class="example_text">
+        <p class="example_text__title">${title}</p>
+        <p class="example_text__author">${author}</p>
+        <div class="example_subtext">
+          <p class="example_subtext__pages">${pages} pages</p>
+          <p class="example_subtext__read">Ready by: ${dateRead}</p>
+        </div>
+      </div>
+    `;
+    example.insertAdjacentHTML('afterbegin', html);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const getData = async (condition) => {
   try {
@@ -23,7 +48,11 @@ const getBooks = (e) => {
   getData(condition);
 };
 
-getData('all');
+const init = () => {
+  getData('all');
+  createHTML();
+};
+init();
 
 hintBtns.forEach((btn) => {
   btn.addEventListener('click', function () {
